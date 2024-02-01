8cc61e45d4ca9e22b230bf7a65452eae20a7c693
George Worsnop <georgeworsnop@gmail.com>
2024-02-01 15:16:59 +0000

    fix(character): style character page for mobile

--- src/components/organisms/CharacterPage.tsx
+++ src/components/organisms/CharacterPage.tsx
@@ -3,67 +3,60 @@ import { useState, useEffect } from "react";
 import { ICharacter } from "@/types/types";
 import { useParams } from "next/navigation";
 import { getCharacterDataById } from "@/app/api/routes/getCharacterDataById";
+import Image from "next/image";
 
 export default function CharacterPage() {
   const [character, setCharacter] = useState<ICharacter | undefined>();
-  const [isLoading, setIsLoading] = useState(false);
 
   const { id } = useParams();
 
   useEffect(() => {
-    setIsLoading(true);
     const fetchData = async () => {
       const data = await getCharacterDataById({ id });
       setCharacter(data);
-      setIsLoading(false);
     };
     fetchData();
   }, []);
 
-  if (isLoading) {
-    return (
-      <div className="w-screen text-center pt-40">
-        <h3 className="text-1xl text-black font-semibold">
-          Character Page Loading ...
-        </h3>
-        <br />
-        <p className="loader"></p>
-      </div>
-    );
-  } else
-    return (
-      <>
-        <div className="w-screen pl-20">
-          {character && (
-            <>
-              <div className="absolute top-56 left-40 flex text-white">
-                <img
-                  className="image rounded-full"
-                  width="200px"
+  return (
+    <>
+      <div className="w-full">
+        {character ? (
+          <>
+            <div className="relative flex flex-col items-center pt-24 text-white md:flex-row md:pl-20 md:pt-44">
+              <div className="flex h-40 w-40 md:h-64 md:w-64">
+                <Image
+                  height="256"
+                  width="256"
                   src={character.avatar}
                   alt={character.name}
-                ></img>
-                <div className="pl-6 pt-4">
-                  <ul>
-                    <li className="flex w-full justify-start pb-6">
-                      <h2 className="font-bold text-5xl">{character.name}</h2>
-                    </li>
-                    <li className="flex w-full justify-start gap-2">
-                      <p className="font-semibold">Status:</p>
-                      <p>{character.status}</p>
-                    </li>
-                    <li className="flex w-full justify-start gap-2">
-                      <p className="font-semibold">Origin:</p>
-                      <p>{character.origin.name}</p>
-                    </li>
-                  </ul>
-                </div>
+                  className="rounded-full"
+                ></Image>
               </div>
-              <div className="pt-20">
-                <h3 className="font-semibold text-3xl pb-4">
+              <div className="pt-4 text-center md:pl-6">
+                <ul>
+                  <li className="flex w-full justify-start pb-3 md:pb-6">
+                    <h2 className="text-3xl font-bold md:text-5xl">
+                      {character.name}
+                    </h2>
+                  </li>
+                  <li className="flex w-full justify-center gap-2 md:justify-start">
+                    <p className="font-semibold">Status:</p>
+                    <p>{character.status}</p>
+                  </li>
+                  <li className="flex w-full justify-center gap-2 md:justify-start">
+                    <p className="font-semibold">Origin:</p>
+                    <p>{character.origin.name}</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="md:pl-4">
+              <div className="pt-10 md:pt-20">
+                <h3 className="pb-4 text-center text-3xl font-semibold md:pl-4 md:text-left">
                   Location Details:
                 </h3>
-                <ul className="flex flex-col gap-1">
+                <ul className="flex flex-col gap-1 pl-4">
                   <li className="flex w-full justify-start gap-2">
                     <p className="font-semibold">Name:</p>
                     <p>{character.location.name}</p>
@@ -83,10 +76,10 @@ export default function CharacterPage() {
                 </ul>
               </div>
               <div className="py-10">
-                <h3 className="font-semibold text-3xl pb-4">
+                <h3 className="pb-4 text-center text-3xl font-semibold md:pl-4 md:text-left">
                   Episodes ({character.episodes.length}):
                 </h3>
-                <ul className="flex flex-col gap-1">
+                <ul className="flex flex-col gap-1 pl-4">
                   <li className="flex w-full justify-start gap-2">
                     <p className="font-semibold">First appearance:</p>
                     <p>{character.episodes[0].name}</p>
@@ -136,9 +129,16 @@ export default function CharacterPage() {
                   </li>
                 </ul>
               </div>
-            </>
-          )}
-        </div>
-      </>
-    );
+            </div>
+          </>
+        ) : (
+          <div className="relative w-screen pt-40 text-center">
+            <h3 className="text-2xl font-semibold text-white">
+              Character Page Loading ...
+            </h3>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
